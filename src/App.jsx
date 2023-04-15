150163c2617ef2a912e5a9cc080cf6f2aebeb370
mukhammadazam <rizayevmuhammadazam9@gmail.com>
2023-04-15 23:47:55 +0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-          <h1 class="text-3xl font-bold underline">
-            Hello world!
-          </h1>
+    <div className='App'>
+      <div className='container'>
+        <h1 class='text-3xl font-bold underline'>Hello world!</h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
